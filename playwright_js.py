7ecbc5b452c084a766e9b7f27e1cb24f19167538
asilin89 <andrei.s8899@gmail.com>
2026-03-29 09:09:25 +0200

    added some pw examples files

---
+++ playwright_js.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # wait_until is used when we need to wait page to be loaded
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#auth/login",
+              wait_until="networkidle")
+
+    new_text = "My New Title"
+
+    # page.evaluate() runs js code
+    page.evaluate(
+        """
+        (text) => {
+            const title = document.getElementById("authentication-ui-course-title-text")
+            title.textContent = text
+        }
+        """,
+        new_text
+    )
+
+    page.wait_for_timeout(2000)
